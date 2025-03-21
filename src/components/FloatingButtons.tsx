bd88356634e2d55098d755fc40c04c58e73f530d
Vanessa <nassangavanessa2@gmail.com>
2025-03-21 20:39:12 +0300

    added theme feature1

--- src/components/FloatingButtons.tsx
+++ src/components/FloatingButtons.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { FaWhatsapp, FaArrowUp } from 'react-icons/fa';
 
 const FloatingButtons = () => {
